d5aa6fd653de8bb2dd7f2a1d9e3997d799a2ae2f
Antonio Carlos <acdona@hotmail.com>
2021-02-28 15:33:14 -0300

    sending view-color

--- adm/app/adms/Views/sitsUser/viewSitsUser.php
+++ adm/app/adms/Views/sitsUser/viewSitsUser.php
@@ -4,31 +4,31 @@ if (!defined('R4F5CC')) {
     die("Erro: Página não encontrada!");
 }
 ?>
+
 <div class="content p-1">
     <div class="list-group-item">
         <div class="d-flex">
             <div class="mr-auto p-2">
-                <h2 class="display-4 title">Detalhes da Cor</h2>
+                <h2 class="display-4 title">Detalhes da Situação para Usuário</h2>
             </div>
             <?php
-            
-            if (!empty($this->data['viewColor'])) {
-                extract($this->data['viewColor'][0]);
+            if (!empty($this->dados['viewSitsUser'])) {
+                extract($this->dados['viewSitsUser'][0]);
                 ?>
                 <div class="p-2">
                     <span class="d-none d-lg-block">
-                        <a href="<?php echo URLADM; ?>list-colors/index" class="btn btn-outline-info btn-sm">Listar</a>
-                        <a href="<?php echo URLADM . 'edit-color/index/' . $id; ?>" class="btn btn-outline-warning btn-sm">Editar</a>
-                        <a href="<?php echo URLADM . 'delete-color/index/' . $id; ?>" class="btn btn-outline-danger btn-sm" data-confirm="Excluir">Apagar</a> 
+                        <a href="<?php echo URLADM; ?>list-sits-users/index" class="btn btn-outline-info btn-sm">Listar</a>
+                        <a href="<?php echo URLADM . 'edit-sits-user/index/' . $id; ?>" class="btn btn-outline-warning btn-sm">Editar</a>
+                        <a href="<?php echo URLADM . 'delete-sits-user/index/' . $id; ?>" class="btn btn-outline-danger btn-sm" data-confirm="Excluir">Apagar</a> 
                     </span>
                     <div class="dropdown d-block d-lg-none">
                         <button class="btn btn-outline-primary btn-sm dropdown-toggle" type="button" id="acoesListar" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             Ações
                         </button>
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="acoesListar">
-                            <a class="dropdown-item" href="<?php echo URLADM; ?>list-colors/index">Listar</a>
-                            <a class="dropdown-item" href="<?php echo URLADM . 'edit-color/index/' . $id; ?>">Editar</a>
-                            <a class="dropdown-item" href="<?php echo URLADM . 'delete-color/index/' . $id; ?>" data-confirm="Excluir">Apagar</a>
+                            <a class="dropdown-item" href="<?php echo URLADM; ?>list-sits-users/index">Listar</a>
+                            <a class="dropdown-item" href="<?php echo URLADM . 'edit-sits-user/index/' . $id; ?>">Editar</a> 
+                            <a class="dropdown-item" href="<?php echo URLADM . 'delete-sits-user/index/' . $id; ?>" data-confirm="Excluir">Apagar</a>
                         </div>
                     </div>
                 </div>
@@ -40,24 +40,20 @@ if (!defined('R4F5CC')) {
             echo $_SESSION['msg'];
             unset($_SESSION['msg']);
         }
-        
-        if (!empty($this->data['viewColor'])) {
+
+        if (!empty($this->dados['viewSitsUser'])) {
             ?>
-            <dl class="row">
+            <dl class="row">                
 
                 <dt class="col-sm-3">ID</dt>
                 <dd class="col-sm-9"><?php echo $id; ?></dd>
 
                 <dt class="col-sm-3">Nome</dt>
-                <dd class="col-sm-9"><?php echo $name; ?></dd>
-
-                <dt class="col-sm-3">Cor</dt>
-                <dd class="col-sm-9"><?php echo $color; ?></dd>
-
+                <dd class="col-sm-9"><span class="badge badge-<?php echo $color; ?>"><?php echo $name; ?></span></dd>
             </dl>
             <?php
         } else {
-            echo "<div class='alert alert-danger' role='alert'>Erro: Cor não encontrada!</div>";
+            echo "<div class='alert alert-danger' role='alert'>Erro: Situação para usuário não encontrado!</div>";
         }
         ?>
     </div>
