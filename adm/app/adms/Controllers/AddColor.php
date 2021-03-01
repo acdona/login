4a9b345c1e365ad1b1b401a6cabc6c1d44318e92
Antonio Carlos <acdona@hotmail.com>
2021-03-01 09:05:38 -0300

    sending add-color

---
+++ adm/app/adms/Controllers/AddColor.php
@@ -0,0 +1,53 @@
+<?php
+namespace App\adms\Controllers;
+
+if (!defined('R4F5CC')) {
+    header("Location: /");
+    die("Erro: Página não encontrada!");
+}
+/**
+ * AddColor Controller responsible for adding a color.
+ *
+ * @version 1.0
+ *
+ * @author Antonio Carlos Doná
+ *
+ * @access public
+ *
+*/
+class AddColor
+{
+    /** @var array $data Receives the data that must be sent to VIEW. */
+    private $data;
+
+    /** @var array $formData Receives form data. */
+    private $formData;
+
+    public function index() {
+
+        $this->formData = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+        if(!empty($this->formData['AddColor'])){
+            unset($this->formData['AddColor']);
+            $createNewColor = new \App\adms\Models\AdmsAddColor();
+            $createNewColor->create($this->formData);
+            if($createNewColor->getResult()) {
+                $urlDestiny = URLADM . "list-colors/index";
+                header("Location: $urlDestiny");
+            } else {
+                $this->data['form'] = $this->formData;
+                $this->viewNewColor();
+            }
+        } else {
+            $this->viewNewColor();
+        }
+    }
+
+       private function viewNewColor() {  
+
+            $carregarView = new \Core\ConfigView("adms/Views/colors/addColor", $this->data);
+            $carregarView->render();   
+       }
+
+}
+
+?>
---
+++ adm/app/adms/Models/AdmsAddColor.php
@@ -0,0 +1,70 @@
+<?php
+namespace App\adms\Models;
+
+if (!defined('R4F5CC')) {
+    header("Location: /");
+    die("Erro: Página não encontrada!");
+}
+
+/**
+ * AdmsAddColor Model responsible for adding a color.
+ *
+ * @version 1.0
+ *
+ * @author Antonio Carlos Doná
+ *
+ * @access public
+ *
+*/
+class AdmsAddColor
+{
+
+    private array $data;
+    private bool $resultado;
+
+    function getResult() {
+        return $this->result;
+    }
+
+    public function create(array $data = null) {
+        $this->data = $data;
+        $valEmptyField = new \App\adms\Models\helper\AdmsValEmptyField();
+        $valEmptyField->validateData($this->data);
+
+        if ($valEmptyField->getResult()) {
+            $this->valInput();
+        } else {
+            $this->result = true;
+        }
+    }
+
+    private function valInput() {
+        $valColor = new \App\adms\Models\helper\AdmsValColor();
+        $valColor->valColor($this->data['name']);
+      
+        if ($valColor->getResult()) {
+            $this->add();
+        } else {
+            $this->result = false;
+        }
+    }
+
+    private function add() {
+        $this->data['name'] = $this->data['name'];
+        $this->data['color'] = $this->data['color'];
+        $this->data['created'] = date("Y-m-d H:i:s");
+        $createColor = new \App\adms\Models\helper\AdmsCreate();
+        $createColor->exeCreate("adms_colors", $this->data);
+
+        if ($createColor->getCreateResult()) {
+            $_SESSION['msg'] = "<div class='alert alert-success' role='alert'>Cor cadastrada com sucesso!</div>";
+            $this->result = true;
+        }else {
+            $_SESSION['msg'] = "<div class='alert alert-warning' role='alert'>Erro: Cor não cadastrada</div>";
+            $this->result = false;
+        }
+
+    }
+}
+
+?>
